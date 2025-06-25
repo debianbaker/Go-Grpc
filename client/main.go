2a22be956f4e37fe360f8d4b799dce0cacd0a428
debianbaker <linuxrocker002@gmail.com>
2025-06-25 15:47:47 +0530

    Unary RPC implemented

---
+++ client/main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+	pb "github.com/debianbaker/Go-Grpc/proto"
+	"log"
+)
+const ( 
+	port = ":8080"
+)
+
+func main(){
+	conn, err := grpc.NewClient("localhost"+port, grpc.WithTransportCredentials(insecure.NewCredentials()))
+	if err != nil{
+		log.Fatalf("Did not connect: %v", err)
+	}
+	defer conn.Close()
+
+	client := pb.NewGreetServiceClient(conn)
+	
+	// names := &pb.NamesList{
+	// 	Names: []string{"Puneet", "Alice", "Bob"},
+	// }
+
+	callSayHello(client)
+}
